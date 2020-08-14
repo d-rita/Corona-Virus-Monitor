63e4079133a042020bef22d5c0dc8b2cd172cb6b
Diana Rita Nanyanzi <31903212+d-rita@users.noreply.github.com>
2020-08-14 06:35:41 +0300

    Implement ranking functionality
    - Convert functional table component to a class component
    - Add order of sorting and column key to component state
    - Add function to toggle order of ranked column between asc and desc
    - Sort data into either ascending or descending order
    - Map sorted data over table rows to be rendered

--- src/components/table.js
+++ src/components/table.js
@@ -2,41 +2,82 @@ import React from "react";
 import Search from "./Search";
 import { formatNumber } from "../utils/formatNumber";
 
-const ResultsTable = (props) => {
-    const { countryData, search } = props;
+class ResultsTable extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            order: "default",
+            key: ''
+        };
 
-    const tableRows = countryData.map((country) => {   
-        return (
-            <tr key={country.CountryCode}>
-                <td>{country.Country}</td>
-                <td>{formatNumber(country.TotalConfirmed)}</td>
-                <td>{formatNumber(country.TotalDeaths)}</td>
-                <td>{formatNumber(country.TotalRecovered)}</td>
-            </tr>
-        );
-    });
+        this.rankTableColumn = this.rankTableColumn.bind(this)
+    }
 
-    return(
-        <div className="container">
-        <Search search={search}/>
-        <div className="countryStatsSection">
-            <table id="statsTable">
-                <caption>Individual Country Statistics</caption>
-                <thead>
-                    <tr>
-                        <th>Country</th>
-                        <th>Cases</th>
-                        <th>Deaths</th>
-                        <th>Recovered</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {tableRows} 
-                </tbody>
-            </table>
+    rankTableColumn(key){
+        const { order } = this.state;
+
+        let newOrder;
+
+        // toggle ranking order
+        if(order === "default") { newOrder = "desc"}
+        if(order === "desc") { newOrder = "asc"}
+        if(order === "asc") { newOrder = "desc"}
+
+        this.setState({
+            order: newOrder,
+            key
+        });
+    };
+
+
+    render(){
+        const { countryData, search } = this.props;
+
+        const { order, key } = this.state;
+
+        const sortedCountryData = countryData.sort((a, b) => {
+            if (a[key] < b[key]) {
+              return order === 'asc' ? -1 : 1;
+            }
+            if (a[key] > b[key]) {
+              return order === 'asc' ? 1 : -1;
+            }
+            return 0;
+        });
+
+        const tableRows = sortedCountryData.map((country) => {   
+            return (
+                <tr key={country.CountryCode}>
+                    <td>{country.Country}</td>
+                    <td>{formatNumber(country.TotalConfirmed)}</td>
+                    <td>{formatNumber(country.TotalDeaths)}</td>
+                    <td>{formatNumber(country.TotalRecovered)}</td>
+                </tr>
+            );
+        });
+
+        return(
+            <div className="container">
+            <Search search={search}/>
+            <div className="countryStatsSection">
+                <table id="statsTable">
+                    <caption>Individual Country Statistics</caption>
+                    <thead>
+                        <tr>
+                            <th onClick={()=>this.rankTableColumn('Country')}>Country</th>
+                            <th onClick={()=>this.rankTableColumn('TotalConfirmed')}>Cases</th>
+                            <th onClick={()=>this.rankTableColumn('TotalDeaths')}>Deaths</th>
+                            <th onClick={()=>this.rankTableColumn('TotalRecovered')}>Recovered</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {tableRows} 
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
-    )
+        )
+    }  
 }
 
 export default ResultsTable;
